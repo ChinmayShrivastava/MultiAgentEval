4176b933896193ab61b14a5b5cb6d8f5bf7313d2
Chinmay Shrivastava <chinmay.shrivastava.1999@gmail.com>
2024-06-04 01:01:47 -0400

    updates

---
+++ agents/Agent.py
@@ -0,0 +1,71 @@
+from llama_index.llms.openai import OpenAI
+from pydantic import BaseModel
+
+from agents.prompts import DEFAULT_AGENT_SPAWN_PROMPT, DEFAULT_AGENT_GENERATED_REASON, DEFAULT_ANSWER_PROMPT
+
+import re
+
+
+DEFAULT_MODEL = 'gpt-4o'
+DEFAULT_AGENTS_TO_SPAWN = 4
+
+class MMLU(BaseModel):
+	question: str
+	answers: list[str]
+	correct: str
+
+class Agent:
+	def __init__(
+			self,
+			llm: OpenAI = None,
+			n_agents: int = None
+			):
+		self.llm = llm or OpenAI(model=DEFAULT_MODEL)
+		self.n_agents = n_agents or DEFAULT_AGENTS_TO_SPAWN
+
+		self._agent_tasks = []
+		self._question = None
+	
+	@property
+	def question(self):
+		return self._question
+	
+	@question.setter
+	def question(self, question: MMLU):
+		self._question = question
+	
+	def get_response(self) -> str:
+		if not self._question:
+			raise ValueError("No question has been set.")
+		mmlu_question = self._question
+		self.spawn_agents(mmlu_question.question, self.n_agents)
+		reasonings = self.collect_reasonings()
+		return self.get_answer(mmlu_question.question, mmlu_question.answers, reasonings)
+
+	def get_answer(self, question: str, answers: list[str], reasonings: list[str]) -> str:
+		_answers = ""
+		for i, answer in enumerate(answers):
+			_answers += f"{chr(65 + i)}. {answer}\n"
+		_prompt = DEFAULT_ANSWER_PROMPT.format(
+			question=question, options=_answers, 
+			information="\n".join(reasonings))
+		res = self.llm.complete(_prompt).text
+		_a = re.search(r"Answer: ([A-D])", res)
+		return _a.group(1)
+
+	def spawn_agents(self, question: str) -> None:
+		_prompt = DEFAULT_AGENT_SPAWN_PROMPT.format(n=self.n_agents, question=question)
+		res = self.llm.complete(_prompt).text
+		self._agent_tasks = res.split("\n")
+
+	def collect_reasonings(self) -> list[str]:
+		_reasonings = []
+		for task in self._agent_tasks:
+			_prompt = DEFAULT_AGENT_GENERATED_REASON.format(question=self._question.question, perspective=task)
+			res = self.llm.complete(_prompt).text
+			_reasonings.append(res)
+		return _reasonings
+
+	def reset(self) -> None:
+		self._agent_tasks = []
+		self._question = None
---
+++ agents/prompts.py
@@ -0,0 +1,66 @@
+DEFAULT_AGENT_PROMPT = (
+	"As an advanced language model, answer the questions given to you.\n"
+	"Each question has 4 possible answers, where 3 are incorrect and 1 is correct.\n"
+	"Your task is to identify the correct answer and return the corresponding letter; A, B, C, or D.\n"
+	"You will get additional information that might help you determine the correct answer.\n"
+	"Use this information to your advantage and answer the questions to the best of your ability.\n"
+	"For each question, just return the letter of the correct answer. No Preambles or Postscripts. Just the letter.\n"
+)
+
+DEFAULT_AGENT_SPAWN_PROMPT = (
+	"As an advanced language model, you are required to break down the given question into {n} unique perspectives.\n"
+	"You will get a question to answer which we need information.\n"
+	"Your task is to break down the question into long tailed queries representing unique perspectives "
+	"that we need to further research to provide a correct answer.\n"
+	"Output each perspective as a separate line.\n"
+	"Generate {n} unique perspectives for the given question.\n"
+	"Question: {question}\n"
+	"Output: \n"
+)
+
+DEFAULT_AGENT_GENERATED_REASON = (
+	"As an advanced language model, your job is to provide best reasing for "
+	"the given question from the required perspective.\n"
+	"The user needs information from different angles to make a final decision on the question.\n"
+	"The original question is a multiple choice question so don't provide the answer.\n"
+	"Provide instead, the characteristics of the correct answer and what is the reasoning behind it.\n"
+	"Generate your reasoning in the following format:\n"
+	"------------------------\n"
+	"For the given question, when looking at it from the perspective of \{perspective\}, "
+	"the correct answer should have the following characteristics: \{characteristics\} "
+	"because \{reasoning\}.\n"
+	"------------------------\n"
+	"Replace \{perspective\} with the perspective you are generating reasoning for.\n"
+	"Replace \{characteristics\} with the characteristics using your own words.\n"
+	"Replace \{reasoning\} with the reasoning behind the characteristics.\n"
+	"Generate reasoning for the given question from the required perspective.\n"
+	"Question: {question}\n"
+	"Perspective: {perspective}\n"
+	"Output: \n"
+)
+
+DEFAULT_ANSWER_PROMPT = (
+	"You are instructed to answer the multiple choice question given to you.\n"
+	"Each question has 4 possible answers, where 3 are incorrect and 1 is correct.\n"
+	"Your task is to identify the correct answer and return the corresponding letter; A, B, C, or D.\n"
+	"Use the relevant information attached to the question to determine the correct answer.\n"
+	"The information is collected from different perspectives to help you make an informed decision.\n"
+	"Answer the question to the best of your ability.\n"
+	"Return the response in the following format:\n"
+	"------------------------\n"
+	"Reasoning: \{reasoning\}\n"
+	"Answer: \{answer\}\n"
+	"------------------------\n"
+	"Replace \{reasoning\} with the reasoning, why you think the answer is correct.\n"
+	"Replace \{answer\} with the letter of the correct answer.\n"
+	"Answer the question with the correct letter.\n"
+	"Question: {question}\n"
+	"Options: {options}\n"
+	"Information: {information}\n"
+	"Output: \n"
+)
+
+# if __name__ == "__main__":
+	# print(DEFAULT_AGENT_PROMPT)
+	# print(DEFAULT_AGENT_SPAWN_PROMPT)
+	# print(DEFAULT_AGENT_GENERATED_REASON)
