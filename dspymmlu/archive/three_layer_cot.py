46cfd58ca87c2454947a84f1abbb60ee10bf3dad
Anthony Humay <ahumay7@gmail.com>
2024-06-16 23:46:09 -0400

    halving, but had to add extra layer CleanUpOptions for output format

--- dspymmlu/archive/three_layer_cot.py
+++ dspymmlu/archive/three_layer_cot.py
@@ -1,15 +1,19 @@
 import json
-
+import os
 import dspy
 import tqdm
 from datahandler import get_data
 from dspy_helpers import dispatch_optmizer
+import re
 
 # CONFIGURE DEFAULTs
 
+# Idk why, but I needed this:
+os.environ["OPENAI_API_KEY"] = ""
+
 SUBJECT = 'anatomy'
 SAVE_PATH = 'testjson.json'
-DEFAULT_MODEL_STRING = 'gpt-3.5-turbo-1106'
+DEFAULT_MODEL_STRING = 'gpt-4o'
 MAX_TOKENS = 256
 # DEFAULT MODEL OBJECT
 
@@ -43,11 +47,29 @@ class Reminders(dspy.Signature):
 
     question = dspy.InputField()
 
-    reminders = dspy.OutputField(desc="The list of three reminders that will help the user remember concepts and better answer the question.")
+    reminders = dspy.OutputField(desc="A reminder that will help the user remember concepts and better answer the question.")
 
 class QAset(dspy.Signature):
     """
-    Given a multiple choice question, the subject, 4 options, and some supplemental information, return the alphabetical letter of the correct answer.
+    Given a question, the subject, 2 answer choices, and some supplemental information, return the alphabetical letter of the correct answer choice.
+    """
+
+    question = dspy.InputField(desc="The multiple choice question.")
+    
+    subject = dspy.InputField(desc="The subject of the question.")
+
+    possibility = dspy.InputField(desc="Possible answer of the question, labeled with the letter it should always be referred to `a`, `b`, `c` or `d`.")
+    other_possibility = dspy.InputField(desc="Another possible answer of the question, labeled with the letter it should always be referred to `a`, `b`, `c` or `d`.")
+
+    core_question = dspy.InputField(desc="The one liner core question.")
+    info = dspy.InputField(desc="The list of atomic information available in the question.")
+    reminders = dspy.InputField(desc="The list of three reminders that will help the user remember concepts and better answer the question.")
+
+    answer = dspy.OutputField(desc="The alphabetical letter of the correct answer; `a`, `b`, `c` or `d`")
+
+class NarrowDownOptions(dspy.Signature):
+    """
+    Given a multiple choice question, the subject, 4 answer choices, and some supplemental information, choose the two best options.
     """
 
     question = dspy.InputField(desc="The multiple choice question.")
@@ -63,7 +85,20 @@ class QAset(dspy.Signature):
     info = dspy.InputField(desc="The list of atomic information available in the question.")
     reminders = dspy.InputField(desc="The list of three reminders that will help the user remember concepts and better answer the question.")
 
-    answer = dspy.OutputField(desc="The alphabetical letter of the correct answer; `a`, `b`, `c` or `d`.")
+    answer = dspy.OutputField(desc="The best two answer choices with their letters, for example [a, c]")
+
+class CleanUpOptions(dspy.Signature):
+    """
+    From text, return an array of size 2 whose values are one of `a`, `b`, `c` or `d`. Or make a guess.
+    """
+    a = dspy.InputField(desc="Option `a` of the question.")
+    b = dspy.InputField(desc="Option `b` of the question.")
+    c = dspy.InputField(desc="Option `c` of the question.")
+    d = dspy.InputField(desc="Option `d` of the question.")
+
+    prev_answers = dspy.InputField(desc="The previously decided best two answer choices.")
+
+    answer = dspy.OutputField(desc="Nothing but an array of the two best answer choice letters. IT IS CRUCIAL YOU OUTPUT SOLELY AN ARRAY OF LENGTH 2 WITH VALUES BEING ONE OF `a`, `b`, `c` or `d`. Examples: [a, c], [b, d], [a, b]")
 
 # METRICS
 
@@ -76,7 +111,14 @@ trainset, _, _ = get_data(SUBJECT)
 
 # PROGRAM
 
-# cot = dspy.ChainOfThought(QAset)
+def parse_array_from_string(input_string):
+    match = re.search(r'\[(.*?)\]', input_string)
+    if match:
+        array_content = match.group(1)
+        array_elements = [element.strip().strip("'\"`") for element in array_content.split(',')]
+        return array_elements
+    else:
+        return None
 
 class COT(dspy.Module):
     def __init__(self):
@@ -85,20 +127,51 @@ class COT(dspy.Module):
         self.core_question = dspy.ChainOfThought(CoreQuestion)
         self.info = dspy.ChainOfThought(ProblemSolvingInfo)
         self.reminders = dspy.ChainOfThought(Reminders)
+        self.narrow_down_options = dspy.ChainOfThought(NarrowDownOptions)
+        self.clean_up_options = dspy.ChainOfThought(CleanUpOptions)
 
         self.prog = dspy.ChainOfThought(QAset)
 
     def forward(self, question, subject, a, b, c, d):
-        return self.prog(
+        core_question = self.core_question(question=question)['core_question']
+        info = self.info(question=question)['info']
+        reminders = self.reminders(question=question)['reminders']
+        answer_map = {'a': a, 'b': b, 'c': c, 'd': d}
+        options = self.narrow_down_options(
             question=question,
             subject=subject,
             a=a,
             b=b,
             c=c,
             d=d,
-            core_question=self.core_question(question=question)['core_question'],
-            info=self.info(question=question)['info'],
-            reminders=self.reminders(question=question)['reminders']
+            core_question=core_question,
+            info=info,
+            reminders=reminders
+        )['answer']
+
+        cleaned_options = self.clean_up_options(
+            a=a,
+            b=b,
+            c=c,
+            d=d,
+            prev_answers=options
+        )['answer']
+
+        remaining = parse_array_from_string(cleaned_options)
+        option_1 = remaining[0]
+        option_2 = remaining[1]
+
+        one_answer = option_1 + ": " + answer_map[option_1]
+        two_answer = option_2 + ": " + answer_map[option_2]
+
+        return self.prog(
+            question=question,
+            subject=subject,
+            possibility=one_answer,
+            other_possibility=two_answer,
+            core_question=core_question,
+            info=info,
+            reminders=reminders
         )
 
 # OPTIMIZER
@@ -158,14 +231,14 @@ class DSPYpipeline:
 
     def load(self, path):
         p = COT()
-        p.load(path=path)
+        #p.load(path=path)
         return p
 
     def test(self, subject):
         _, _, testset = get_data(subject)
         model = self.load(self.save_path)
         responses = {}
-        for example in tqdm.tqdm(testset):
+        for example in tqdm.tqdm(testset[:20]):
             answer = model.forward(
                 question=example.question,
                 subject=example.subject,
@@ -174,6 +247,7 @@ class DSPYpipeline:
                 c=example.c,
                 d=example.d
             )
+            # self.lm.inspect_history(n=5)
             responses[example.question] = {
                 "answer": answer['answer'],
                 "rationale": answer['rationale'],
@@ -193,13 +267,12 @@ if __name__ == '__main__':
         save_path=save_path,
         max_tokens=MAX_TOKENS
     )
-    pipeline.optimize(subject, optimizer=optimizer)
-    # # test
-    # responses = pipeline.test(subject)
-    # pring the score
-    # # correct = sum([1 for k, v in responses.items() if v['correct']])
-    # # total = len(responses)
-    # # print(f"Accuracy: {correct/total}")
-    # # # save responses
-    # # with open(f"{_save_path}{subject}_{optimizer}_responses.json", 'w') as f:
-    #     json.dump(responses, f)
+    # pipeline.optimize(subject, optimizer=optimizer)
+    # test
+    responses = pipeline.test(subject)
+    correct = sum([1 for k, v in responses.items() if v['correct']])
+    total = len(responses)
+    print(f"Accuracy: {correct/total}")
+    # save responses
+    with open(f"{_save_path}{subject}_{optimizer}_responses.json", 'w') as f:
+        json.dump(responses, f)
